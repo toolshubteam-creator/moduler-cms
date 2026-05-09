6896aefc7c14b13159579cee0a586a79978b8c9d
toolshubteam-creator <toolshubteam@gmail.com>
2026-05-09 15:33:41 +0300

    feat: faz-1.1 solution iskeleti ve nuget paketleri

---
+++ src/Cms.Web/Program.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture);
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ tests/Cms.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Cms.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
